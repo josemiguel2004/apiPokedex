5965bf84b7c4b945c7e5c2cbcb0be399bfecb1f4
josemiguel2004 <miguel.zico@outlook.com>
2022-08-04 15:03:43 -0300

    Método para buscar um pokemon por vez

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -15,12 +15,24 @@ export class Tab1Page implements OnInit {
   constructor(public pokemonService: PokemonService) {}
 
   ngOnInit() {
+    this.buscarPokemon();
+  }
+
+  async buscarPokemon() {
     this.pokemonService.buscarTodosPokemon().subscribe((dados) => {
-      this.total = dados['count'];
+      this.total = dados['total'];
       this.next = dados['next'];
       this.previous = dados['previous'];
-      this.listaPokemon = dados['results'];
+      //  this.listaPokemon = dados['results'];
       console.log(dados);
+
+      for (let pokemon of dados['results']) {
+        this.pokemonService
+          .buscarUmPokemon(pokemon.url)
+          .subscribe((dadosPokemon) => {
+            this.listaPokemon.push(dadosPokemon);
+          });
+      }
     });
   }
 }
